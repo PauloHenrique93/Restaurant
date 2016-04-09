bb892c7ad193dd747f5d269a5b3390118fb4799b
paulo <paulohenrique.de93@hotmail.com>
2016-04-09 00:33:33 -0300

    for security

--- UMain.pas
+++ UMain.pas
@@ -6,6 +6,28 @@ uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, ExtCtrls, StdCtrls;
 
+
+  
+//========================== TFORM1 CLASS =================================
+type
+  TForm1 = class(TForm)
+    restaurantPanel: TPanel;
+    addLocalButton: TButton;
+    localScrollBox: TScrollBox;
+    localNameEdit: TEdit;
+    newLocalButton: TButton;
+    procedure addLocalButtonClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure newLocalButtonClick(Sender: TObject);
+    procedure localNameEditKeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+
 //=========================== LOCAL CLASS ===================================
 type
   TLocal = class(TGroupBox)
@@ -14,26 +36,9 @@ type
     { Private declarations }
   public
     { Public declarations }
-    constructor create(sender:TComponent);override;
-    procedure insertTable(sender: TObject);
-
-  end;
-
-//========================== TFORM1 CLASS =================================
-type
-  TForm1 = class(TForm)
-    restaurantPanel: TPanel;
-    addLocalButton: TButton;
-    localScrollBox: TScrollBox;
-    localNameEdit: TEdit;
-    Button1: TButton;
-    procedure addLocalButtonClick(Sender: TObject);
-    procedure FormShow(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
-  private
-    { Private declarations }
-  public
-    { Public declarations }
+    tableScrollBox: TScrollBox;
+    procedure insertTable(Sender: TObject);
+    constructor create(Sender:TComponent);override;
   end;
 
 
@@ -56,11 +61,15 @@ type
     { Private declarations }
   public
     { Public declarations }
+     constructor create(Sender:TComponent);override;
   end;
 
 var
   Form1: TForm1;
   already: Integer;
+  alreadyScrollTable;
+  localTop: Integer;
+  localSpace: Integer;
   precedentLocalTop: Integer;
   precedentLocalLeft: Integer;
 
@@ -76,30 +85,32 @@ begin
   local:= TLocal.Create(localScrollBox);
   local.Parent:= localScrollBox;
   local.Caption:= localNameEdit.Text;
-  //local.OnClick:= insertTable();
+  local.OnClick:= local.insertTable;
 
+  //IMPRIMI O PRIMEIRO ELEMENTO GROUPBOX INSERIDO
   if (already = 0) then
   begin
-    local.Top:= 15;
-    local.Left:= 15;
+    local.Top:= localTop;
+    local.Left:= localSpace;
     precedentLocalTop:= local.Top;
     precedentLocalLeft:= local.Left;
 
     already:= 1;
   end
 
-  else if ( (precedentLocalLeft + local.Width + local.Left + 15) >= ((localScrollBox.Width) - 30) ) then
+  //IMPRIMI O PRIMEIRO ELEMENTO BROUPBOX DA LINHA ABAIXO
+  else if ( (precedentLocalLeft + local.Width + local.Left + localSpace) >= ((localScrollBox.Width) - 30) ) then
   begin
     local.Top:= precedentLocalTop + local.Height;
-    local.Left:= 15;
+    local.Left:= localSpace;
     precedentLocalTop:= local.Top;
     precedentLocalLeft:= local.Left;
   end
-
+  //IMPRIMI O ELEMENTO GROUPBOX NA MESMA LINHA
   else
   begin
     local.Top:= precedentLocalTop;
-    local.Left:= (precedentLocalLeft + local.Width + 15);
+    local.Left:= (precedentLocalLeft + local.Width + localSpace);
     precedentLocalLeft:= local.Left;
     precedentLocalTop:= local.Top;
   end;
@@ -119,13 +130,16 @@ end;
 
 procedure TForm1.FormShow(Sender: TObject);
 begin
+  localTop:= 15;
+  localSpace:= 15;
   already:= 0;
+  alreadyScrollTable:= 0;
   localNameEdit.Visible:= false;
   addLocalButton.Visible:= false;
 end;
 
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.newLocalButtonClick(Sender: TObject);
 begin
   localNameEdit.Visible:= true;
   addLocalButton.Visible:= true;
@@ -133,10 +147,7 @@ end;
 
 { TLocal }
 
-
-
-
-constructor TLocal.create(sender: TComponent);
+constructor TLocal.create(Sender: TComponent);
 begin
    inherited;
   Height:= 185;
@@ -144,9 +155,38 @@ begin
 end;
 
 
-procedure TLocal.insertTable(sender: TObject);
+procedure TLocal.insertTable(Sender: TObject);
+var
+  table: TTable;
 begin
-//
+   if (alreadyScrollTable = 0) then
+   begin
+      tableScrollBox:= TScrollBox.Create((Sender as TGroupbox));
+      tableScrollBox.Parent:= (Sender as TGroupbox);
+      tableScrollBox.Width:= (Sender as TGroupbox).Width;
+      tableScrollBox.Height:= (Sender as TGroupbox).Height;
+   end;
+
+   table:= TTable.Create(tableScrollBox);
+   table.Parent:= (tableScrollBox);
+
+end;
+
+procedure TForm1.localNameEditKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if key = 13 then
+  addLocalButton.Click;
+end;
+
+{ TTable }
+
+constructor TTable.create(Sender: TComponent);
+begin
+  inherited;
+  width:= 40;
+  height:= 40;
+  brush.Color:= Rgb(76,255,85);
 end;
 
 end.
